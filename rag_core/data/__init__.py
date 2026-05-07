2132e0b45619b25126990505115e8c9b578f71e7
corner123 <wubuju1127@gmail.com>
2026-05-07 20:35:22 +0800

    feat: add data loaders for markdown, text, pdf, code, csv, image

---
+++ rag_core/data/__init__.py
@@ -0,0 +1,4 @@
+from .base import DataLoader
+from .loaders import MarkdownLoader, TextLoader, PDFLoader, CodeLoader, StructuredDataLoader, ImageLoader, LoaderFactory
+
+__all__ = ["DataLoader", "MarkdownLoader", "TextLoader", "PDFLoader", "CodeLoader", "StructuredDataLoader", "ImageLoader", "LoaderFactory"]
